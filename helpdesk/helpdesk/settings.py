e85465f9cfbca48c2c4a1e0a4911b77c4fc4611c
Aniketchoudhary34 <aniketchoudhary3435@gmail.com>
2026-03-26 12:26:18 +0530

    setting

--- helpdesk/helpdesk/settings.py
+++ helpdesk/helpdesk/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!8cy#xgioa*a&j+=8kr5)1o+tucqrou0m$v7_h8l7^)inc#upj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
